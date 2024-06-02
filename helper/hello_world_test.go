af2c58283aa9a30c2c3ad4c22317184088db66e1
Bayu Sugiantoro <bayusugiantoro5@gmail.com>
2024-06-02 16:35:51 +0700

    membatalkan test

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -2,11 +2,21 @@ package helper
 
 import (
 	"fmt"
+	"runtime"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
+
+func TestSkip(t *testing.T) {
+	if runtime.GOOS == "windows" {
+		t.Skip("Can not run on Windows OS")
+	}
+
+	result := HelloWorld("Bayu")
+	require.Equal(t, "Hello Bayu", result, "Result must be 'Hello Bayu'")
+}
 
 func TestHelloWorldRequire(t *testing.T) {
 	result := HelloWorld("Bayu")
