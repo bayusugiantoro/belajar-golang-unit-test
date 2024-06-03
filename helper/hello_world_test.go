6018e5d6a3a691f9a11b3fe31df978508cfa6db2
Bayu Sugiantoro <bayusugiantoro5@gmail.com>
2024-06-03 22:56:37 +0700

    table benchmark

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -9,6 +9,38 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func BenchmarkTable(b *testing.B) {
+	benchmarks := []struct {
+		name    string
+		request string
+	}{
+		{
+			name:    "Eko",
+			request: "Eko",
+		},
+		{
+			name:    "Kurniawan",
+			request: "Kurniawan",
+		},
+		{
+			name:    "EkoKurniawanKhannedy",
+			request: "Eko Kurniawan Khannedy",
+		},
+		{
+			name:    "Bayu",
+			request: "Bayu Sugiantoro",
+		},
+	}
+
+	for _, benchmark := range benchmarks {
+		b.Run(benchmark.name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				HelloWorld(benchmark.request)
+			}
+		})
+	}
+}
+
 func BenchmarkSub(b *testing.B) {
 	b.Run("Bayu", func(b *testing.B) {
 		for i := 0; i < b.N; i++ {
