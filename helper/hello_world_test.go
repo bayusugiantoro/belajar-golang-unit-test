8f22209dc091b5255b7b5c509852702f88a7a044
Bayu Sugiantoro <bayusugiantoro5@gmail.com>
2024-06-03 22:49:15 +0700

    sub benchmark

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -9,6 +9,19 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func BenchmarkSub(b *testing.B) {
+	b.Run("Bayu", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			HelloWorld("Bayu")
+		}
+	})
+	b.Run("Sugiantoro", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			HelloWorld("Sugiantoro")
+		}
+	})
+}
+
 func BenchmarkHelloWorld(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		HelloWorld("Bayu")
